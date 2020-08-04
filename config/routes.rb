4f14ed2dda553bf05f012132a6ae23ef4dd6650c
“Aria <a.c.nikzad@gmail.com>
2020-08-03 17:36:44 -0700

    added user controller/model

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     resources :memes 
+    post "/users" => "users#create"
   end
+
 
 end
